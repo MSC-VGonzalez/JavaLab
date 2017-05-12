502eef897214aae4fed4ff1e217bc56e11e00eff
MsC-VGonzalez <vgonzalez.general@gmail.com>
2017-05-11 19:24:49 -0500

    Corrección para la lectura de una línea mediante teclado.

--- Readers.java
+++ Readers.java
@@ -2,18 +2,20 @@ import java.io.*;
 
 public class Readers{
 	public static void main(String args[]) throws IOException {
-      InputStreamReader cin = null;
-
-      try {
-         cin = new InputStreamReader(System.in);
-         System.out.println("Enter characters, 'q' to quit.");
-         String s = new String();
-         s = (String) cin.read();
-         System.out.print(s);
-      }finally {
-         if (cin != null) {
-            cin.close();
-         }
-      }
-  }
+		InputStreamReader cin = null;
+		BufferedReader br = null;
+		
+		try {
+			cin = new InputStreamReader(System.in);
+			br = new BufferedReader(cin);
+			String s = new String();
+			System.out.println("Ingrese una línea: ");
+			s = br.readLine();
+			System.out.print(s);
+		}finally {
+			if (cin != null) {
+			   cin.close();
+			}
+		}
+	}
 }
