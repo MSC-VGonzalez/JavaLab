352c298d154cb566652e68c6af18778b098f34aa
MsC-VGonzalez <vgonzalez.general@gmail.com>
2017-05-15 16:55:15 -0500

    Solución al problema número 001 para calentamiento en lenguaje Java.

--- Warmup-1/001. Sleep-In/SleepIn.java
+++ Warmup-1/001. Sleep-In/SleepIn.java
@@ -1,5 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class SleepIn{
-	public boolean sleepIn(boolean weekday, boolean vacation){
+	public static void main (String args[]){
+		Boolean weekday   = false;
+		Boolean vacation  = false;
+		Boolean rest_flag = false;
+		Scanner buffer  = new Scanner(System.in);
+		
+		System.out.println("Ingrese el valor de la variable día de la semana: ");
+		weekday  = buffer.nextBoolean();
+		System.out.println("Ingrese el valor de la variable vacaciones: ");
+		vacation = buffer.nextBoolean();
+		
+		rest_flag = sleepIn(weekday,vacation);
+		
+		if (rest_flag){
+			System.out.println("Es posible dormir un poco más.");
+		}else{
+			System.out.println("No es posible dormir un poco más.");
+		}
+	}
+	
+	public static boolean sleepIn(boolean weekday, boolean vacation){
 		if (vacation == true){
 			return true;
 		}
